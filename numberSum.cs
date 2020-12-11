0d42b6f8ee21874457adf101a75423e9f91a3590
jyrilundin <jyri.lundin@gmail.com>
2020-12-11 15:06:15 +0200

    Gitin testailua.

---
+++ numberSum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace numberSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   int readNumber=0;
+            int readSecondNumber=0;
+            int sum = 0;
+            
+            Console.WriteLine("Give first number:");
+            readNumber = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Give the second number:");
+            readSecondNumber = Convert.ToInt32(Console.ReadLine());
+            if (readNumber > readSecondNumber) {
+                Console.WriteLine(readNumber + " is " + " bigger "+"than "+readSecondNumber);
+            } else if (readNumber < readSecondNumber) {
+                Console.WriteLine(readNumber + " is " + " smaller "+"than "+readSecondNumber);
+            } else {
+                Console.WriteLine("Both numbers are equal!\n");
+            }
+            
+            sum = readNumber + readSecondNumber;
+            Console.WriteLine("The sum of numbers is " + sum+ ".");
+        }
+    }
+}
