0ea34d06aca6bd259e40b711d8fe2f89047c192b
jyrilundin <jyri.lundin@gmail.com>
2020-12-11 15:02:57 +0200

    Gitin testausta.

---
+++ guessGames.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace guessGame
+{
+    class Program
+    {
+        static void Main(string[] args) {
+               
+            int fails = 0;
+            int tries=3;
+            Boolean won = false;
+            Random rand = new Random();
+
+            while (tries > fails && won==false) {
+
+                int random = rand.Next(0,10);
+                Console.WriteLine("Guess the number:");
+                int number = int.Parse(Console.ReadLine());
+
+                if (fails < tries && number!=random) {
+                    Console.WriteLine("You guessed wrong! Try again!");
+                    fails++;
+                }   else if (fails < tries && number==random) {
+                    won = true;
+                }     
+            } 
+
+            if (fails < tries) {
+                Console.WriteLine("You won!");
+            } else {
+                Console.WriteLine("You lost!");
+            }
+        }
+    }
+}
